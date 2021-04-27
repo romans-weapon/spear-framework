5de97bcff57bd62decbcf9251cc20579b6ceceb8
AnudeepKonaboina <59328701+AnudeepKonaboina@users.noreply.github.com>
2021-04-27 19:11:37 +0530

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,31 @@
 #! /bin/bash
 
 set -e
+
+cd spear-framework/ || { echo "ERROR:No such directory"
+exit 1
+}
+
 # build hive-postgres metastore image
 {
   echo "[$(date)]        INFO:[+]Building image: hive-metastore"
-  docker build . -t hive-metastore:latest -f hive-metastore/Dockerfile
+  docker build . -t hive-metastore:latest -f bootstrap/spark-with-hadoop/hive-metastore/Dockerfile
 } && echo "[$(date)]        INFO:[+]Building image hive-metastore: SUCCESS" || {
   echo "[$(date)]        ERROR:[+]Building image for hive-metastore :FAILED"
   exit 1
 }
 
-# build hive-postgres metastore image
+# build spark-hadoop standalone metastore image
 {
-  echo "[$(date)]        INFO:[+]Building image: spark-standalone"
-  docker build . -t spark-standalone-hadoop:latest -f spark-hadoop-standalone/Dockerfile
-} && echo "[$(date)]        INFO:[+]Building image spark-standalone: SUCCESS" || {
-  echo "[$(date)]        ERROR:[+]Building image for spark-standalone :FAILED"
+  echo "[$(date)]        INFO:[+]Building image: spark-hadoop-standalone"
+  docker build . -t spark-standalone-hadoop:latest -f bootstrap/spark-with-hadoop/spark-hadoop-standalone/Dockerfile
+} && echo "[$(date)]        INFO:[+]Building image spark-hadoop-standalone: SUCCESS" || {
+  echo "[$(date)]        ERROR:[+]Building image for spark-hadoop-standalone :FAILED"
   exit 1
 }
 
 #spinning docker containers using compose
-docker-compose up -d 
+docker-compose -f bootstrap/spark-with-hadoop/docker-compose.yml up -d
 
 sleep 60
 #starting hadoop
@@ -29,4 +34,3 @@ docker exec -it spark bash -c "hdfs namenode -format && start-dfs.sh && hdfs dfs
 docker exec -d spark bash -c "hive --service metastore && hive --service hiveserver2"
 docker exec -it spark bash -c "cd /opt && git clone git@github.com:AnudeepKonaboina/spear-framework.git && cd spear-framework && sbt package"
 docker exec -it spark bash -c "cd /opt/spear-framework/target/scala-2.12 && spark-shell --jars spear-framework_2.12-0.1.jar --packages  "
-
