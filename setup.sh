49b356d27e1f9c2d571313e1430f4f5b9123b955
AnudeepKonaboina <krantianudeep@gmail.com>
2021-05-24 09:29:01 +0530

    fixed conficts

--- setup.sh
+++ setup.sh
@@ -1,36 +1,17 @@
 #! /bin/bash
 
 set -e
-
-cd spear-framework/ || { echo "ERROR:No such directory"
-exit 1
-}
-
-# build hive-postgres metastore image
+echo "[$(date)]        INFO:[+]Spear-framework setup status     [started]"
 {
-  echo "[$(date)]        INFO:[+]Building image: hive-metastore"
-  docker build . -t hive-metastore:latest -f bootstrap/spark-with-hadoop/hive-metastore/Dockerfile
-} && echo "[$(date)]        INFO:[+]Building image hive-metastore: SUCCESS" || {
-  echo "[$(date)]        ERROR:[+]Building image for hive-metastore :FAILED"
-  exit 1
-}
-
-# build spark-hadoop standalone metastore image
-{
-  echo "[$(date)]        INFO:[+]Building image: spark-hadoop-standalone"
-  docker build . -t spark-standalone-hadoop:latest -f bootstrap/spark-with-hadoop/spark-hadoop-standalone/Dockerfile
-} && echo "[$(date)]        INFO:[+]Building image spark-hadoop-standalone: SUCCESS" || {
-  echo "[$(date)]        ERROR:[+]Building image for spark-hadoop-standalone :FAILED"
-  exit 1
-}
-
-#spinning docker containers using compose
-docker-compose -f bootstrap/spark-with-hadoop/docker-compose.yml up -d
-
-sleep 60
-#starting hadoop
-echo "Starting services"
-docker exec -it spark bash -c "hdfs namenode -format && start-dfs.sh && hdfs dfs -mkdir -p /tmp && hdfs dfs -mkdir -p /user/hive/warehouse && hdfs dfs -chmod g+w /user/hive/warehouse" &&
-docker exec -d spark bash -c "hive --service metastore && hive --service hiveserver2"
-docker exec -it spark bash -c "cd /opt && git clone git@github.com:AnudeepKonaboina/spear-framework.git && cd spear-framework && sbt package"
-docker exec -it spark bash -c "cd /opt/spear-framework/target/scala-2.12 && spark-shell --jars spear-framework_2.12-0.1.jar --packages  "
+  docker-compose up -d && sleep 60 &&
+    #starting hadoop and hive services
+    docker exec -it spark bash -c "hdfs namenode -format && start-dfs.sh && hdfs dfs -mkdir -p /tmp && hdfs dfs -mkdir -p /user/hive/warehouse && hdfs dfs -chmod g+w /user/hive/warehouse" &&
+    docker exec -it spark bash -c "sed '90 a figlet -f slant -w 100 Spear Framework' /usr/bin/spark-2.4.7-bin-without-hadoop/bin/spark-shell > /usr/bin/spark-2.4.7-bin-without-hadoop/bin/spark-shell-test && yum install -y epel-release figlet && yes | cp /usr/bin/spark-2.4.7-bin-without-hadoop/bin/spark-shell-test /usr/bin/spark-2.4.7-bin-without-hadoop/bin/spark-shell" &&
+    docker exec -d spark bash -c "hive --service metastore && hive --service hiveserver2" &&
+    docker exec -it spark bash -c "chmod u+x /root/spear-shell.sh && wget https://mirrors.estointernet.in/apache/kafka/2.7.0/kafka_2.13-2.7.0.tgz -O /tmp/kafka.tgz && cd / && tar -xvf /tmp/kafka.tgz -C / && mv kafka_2.13-2.7.0 kafka && rm -f /etc/yum.repos.d/bintray-rpm.repo &&  curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo && mv sbt-rpm.repo /etc/yum.repos.d/ && yum install -y sbt && cd /opt && git clone https://github.com/AnudeepKonaboina/spear-framework.git && cd spear-framework && sbt 'set test in assembly := {}' clean assembly" &&
+    echo "[$(date)]        INFO:[+]Spear-framework setup status     [success]"
+} ||
+  {
+    echo "[$(date)]        INFO:[+]Spear-framework setup status      [failed]"
+    exit 1
+  }
