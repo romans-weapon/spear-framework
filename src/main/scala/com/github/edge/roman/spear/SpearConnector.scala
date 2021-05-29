1d13c6695039d896384a686d3102011a0d4cc8ac
AnudeepKonaboina <krantianudeep@gmail.com>
2021-05-29 18:21:56 +0530

    Mongo source no sql

--- src/main/scala/com/github/edge/roman/spear/SpearConnector.scala
+++ src/main/scala/com/github/edge/roman/spear/SpearConnector.scala
@@ -3,7 +3,7 @@ package com.github.edge.roman.spear
 import com.github.edge.roman.spear.connectors.AbstractConnector
 import com.github.edge.roman.spear.commons.SpearCommons
 import com.github.edge.roman.spear.connectors.targetFS.{FStoFS, FiletoFS, JDBCtoFS}
-import com.github.edge.roman.spear.connectors.targetjdbc.{FiletoJDBC, JDBCtoJDBC}
+import com.github.edge.roman.spear.connectors.targetjdbc.{FiletoJDBC, JDBCtoJDBC, NOSQLtoJDBC}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
@@ -43,6 +43,7 @@ object SpearConnector {
         case (SpearCommons.File, SpearCommons.FileSystem) => new FiletoFS(sourceFormat, destFormat)
         case (SpearCommons.Relational, SpearCommons.FileSystem) => new JDBCtoFS(sourceFormat, destFormat)
         case (SpearCommons.FileSystem, SpearCommons.FileSystem) => new FStoFS(sourceFormat, destFormat)
+        case (SpearCommons.NoSql, SpearCommons.Relational) => new NOSQLtoJDBC(sourceFormat, destFormat)
         case (_, _) => throw new Exception(SpearCommons.InvalidParams)
       }
     }
---
+++ src/main/scala/com/github/edge/roman/spear/connectors/targetjdbc/NOSQLtoJDBC.scala
@@ -0,0 +1,37 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.edge.roman.spear.connectors.targetjdbc
+
+import com.github.edge.roman.spear.Connector
+import com.github.edge.roman.spear.commons.SpearCommons
+import com.github.edge.roman.spear.connectors.AbstractTargetJDBCConnector
+import com.mongodb.spark.config.ReadConfig
+import com.mongodb.spark.sql.SparkSessionFunctions
+
+class NOSQLtoJDBC(sourceFormat: String, destFormat: String) extends AbstractTargetJDBCConnector(sourceFormat, destFormat) {
+  override def source(sourceObject: String, params: Map[String, String]): Connector = {
+    logger.info(s"Connector to Target: JDBC with Format: ${destFormat} from Source Object: ${sourceObject} with Format: ${sourceFormat} started running!!")
+    val _df = SparkSessionFunctions(this.df.sparkSession).loadFromMongoDB(ReadConfig(Map("uri" -> params.get("uri").toString.concat(s".$sourceObject"))))
+    this.df = _df
+    logger.info(s"Reading source object: ${sourceObject} with format: ${sourceFormat} status:${SpearCommons.SuccessStatus}")
+    if (this.verboseLogging) this.df.show(this.numRows, false)
+    this
+  }
+}
