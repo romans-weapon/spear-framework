9922a982b76059dc514725350dabbbafd9e3bd0c
AnudeepKonaboina <krantianudeep@gmail.com>
2021-06-08 09:55:58 +0530

    added scope for mutlitarget while defining connecor obj

--- src/main/scala/com/github/edge/roman/spear/SpearConnector.scala
+++ src/main/scala/com/github/edge/roman/spear/SpearConnector.scala
@@ -1,7 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.github.edge.roman.spear
 
 import com.github.edge.roman.spear.connectors.AbstractConnector
 import com.github.edge.roman.spear.commons.SpearCommons
+import com.github.edge.roman.spear.connectors.targetAny.{FiletoAny, JDBCtoAny, NOSQLtoAny}
 import com.github.edge.roman.spear.connectors.targetFS.{FStoFS, FiletoFS, JDBCtoFS, NOSQLtoFS}
 import com.github.edge.roman.spear.connectors.targetNoSQL.{FilettoNoSQL, JDBCtoNoSQL, NoSQLtoNoSQL}
 import com.github.edge.roman.spear.connectors.targetjdbc.{FiletoJDBC, JDBCtoJDBC, NOSQLtoJDBC}
@@ -20,10 +40,10 @@ object SpearConnector {
 
   //companion class spear-connector
   class SpearConnector {
-    private var sourceType: String = _
-    private var sourceFormat: String = _
-    private var destType: String = _
-    private var destFormat: String = _
+    private var sourceType: String = SpearCommons.Star
+    private var sourceFormat: String = SpearCommons.Star
+    private var destType: String = SpearCommons.Star
+    private var destFormat: String = SpearCommons.Star
 
     def source(sourceType: String, sourceFormat: String): SpearConnector = {
       this.sourceType = sourceType
@@ -34,6 +54,11 @@ object SpearConnector {
     def target(targetType: String, targetFormat: String): SpearConnector = {
       this.destType = targetType
       this.destFormat = targetFormat
+      this
+    }
+
+    def multiTarget: SpearConnector = {
+      this.destType = SpearCommons.Star
       this
     }
 
@@ -49,6 +74,9 @@ object SpearConnector {
         case (SpearCommons.File, SpearCommons.NoSql) => new FilettoNoSQL(sourceFormat, destFormat)
         case (SpearCommons.Relational, SpearCommons.NoSql) => new JDBCtoNoSQL(sourceFormat, destFormat)
         case (SpearCommons.NoSql, SpearCommons.NoSql) => new NoSQLtoNoSQL(sourceFormat, destFormat)
+        case (SpearCommons.File, SpearCommons.Star) => new FiletoAny(sourceFormat)
+        case (SpearCommons.Relational, SpearCommons.Star) => new JDBCtoAny(sourceFormat)
+        case (SpearCommons.NoSql, SpearCommons.Star) => new NOSQLtoAny(sourceFormat)
         case (_, _) => throw new Exception(SpearCommons.InvalidParams)
       }
     }
