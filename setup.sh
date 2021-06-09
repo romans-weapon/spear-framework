a1e4f3121bb1d7f3330acb5f65a31f2452980526
AnudeepKonaboina <krantianudeep@gmail.com>
2021-06-09 19:28:31 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,11 @@ echo "[$(date)]        INFO:[+]Spear-framework setup status     [started]"
     docker exec -d spear bash -c "hive --service metastore && sleep 15 && hive --service hiveserver2 && sleep 20 && hive -e 'set mapreduce.framework.name=local;'" &&
     docker exec -it spear bash -c "chmod u+x /root/spear-shell.sh && wget https://mirrors.estointernet.in/apache/kafka/2.7.0/kafka_2.13-2.7.0.tgz -O /tmp/kafka.tgz && cd / && tar -xvf /tmp/kafka.tgz -C / && mv kafka_2.13-2.7.0 kafka && rm -f /etc/yum.repos.d/bintray-rpm.repo &&  curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo && mv sbt-rpm.repo /etc/yum.repos.d/ && yum install -y sbt && cd /opt && git clone https://github.com/AnudeepKonaboina/spear-framework.git && cd spear-framework && sbt 'set test in assembly := {}' clean assembly && cp /etc/jars/* /opt/spear-framework/target/scala-*/" &&
     echo "[$(date)]        INFO:[+]Spear-framework setup status     [success]"
+} && {
+  echo "Next steps:"
+  echo "==========="
+  echo "  1.ssh into the spear-container using the command  : docker exec -it spear bash "
+  echo "  2.Start spear-framework on spark using the command: spear-shell"
 } ||
   {
     echo "[$(date)]        INFO:[+]Spear-framework setup status      [failed]"
